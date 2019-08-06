81af2fbc1f56cfe5e566681f78bb65347996afd4
anyhowstep <anyhowstep@hotmail.com>
2019-08-06 04:36:43 -0400

    Added basic where clause test

---
+++ test/compile-time/expected-output/query/where/test-use-invalid-column.d.ts
@@ -0,0 +1,37 @@
+import * as tm from "type-mapping/fluent";
+import * as tsql from "../../../../../dist";
+export declare const query: tsql.Query<{
+    fromClause: tsql.IFromClause<{
+        outerQueryJoins: undefined;
+        currentJoins: readonly import("@tsql/tsql/dist/join").Join<{
+            readonly tableAlias: "myTable";
+            readonly nullable: false;
+            readonly columns: {
+                readonly myTableId: import("@tsql/tsql/dist/column").Column<{
+                    tableAlias: "myTable";
+                    columnAlias: "myTableId";
+                    mapper: tm.Mapper<unknown, bigint>;
+                }>;
+            };
+            readonly originalColumns: {
+                readonly myTableId: import("@tsql/tsql/dist/column").Column<{
+                    tableAlias: "myTable";
+                    columnAlias: "myTableId";
+                    mapper: tm.Mapper<unknown, bigint>;
+                }>;
+            };
+            readonly primaryKey: undefined;
+            readonly deleteEnabled: true;
+            readonly mutableColumns: readonly [];
+        }>[];
+    }>;
+}>;
+declare type AllowedUsedRef = tsql.FromClauseUtil.AllowedUsedRef<typeof query["fromClause"], {
+    isLateral: true;
+}>;
+declare type AllowedExpr = tsql.IExpr<{
+    mapper: () => boolean;
+    usedRef: AllowedUsedRef;
+}>;
+export declare const notAllowed: AllowedExpr;
+export {};
