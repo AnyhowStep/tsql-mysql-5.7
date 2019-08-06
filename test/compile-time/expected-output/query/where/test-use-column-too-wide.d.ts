1a429bbe9092c04114aef739495df9486ff7beca
anyhowstep <anyhowstep@hotmail.com>
2019-08-06 04:46:47 -0400

    Added more WHERE clause tests

---
+++ test/compile-time/expected-output/query/where/test-use-column-too-wide.d.ts
@@ -0,0 +1,44 @@
+import * as tm from "type-mapping/fluent";
+import * as tsql from "../../../../../dist";
+export declare const query: tsql.Query<{
+    fromClause: tsql.IFromClause<{
+        outerQueryJoins: undefined;
+        currentJoins: readonly import("@tsql/tsql/dist/join").Join<{
+            readonly tableAlias: "myTable";
+            readonly nullable: false;
+            readonly columns: {
+                readonly myTableId: import("@tsql/tsql/dist/column").Column<{
+                    tableAlias: "myTable";
+                    columnAlias: "myTableId";
+                    mapper: tm.Mapper<unknown, bigint>;
+                }>;
+            }; /**
+             * We make this `WHERE` clause handle `myTableId`, even if it is `null`.
+             * So, `bigint|null`.
+             */
+            readonly originalColumns: {
+                readonly myTableId: import("@tsql/tsql/dist/column").Column<{
+                    tableAlias: "myTable";
+                    columnAlias: "myTableId";
+                    mapper: tm.Mapper<unknown, bigint>;
+                }>;
+            };
+            readonly primaryKey: undefined;
+            readonly deleteEnabled: true;
+            readonly mutableColumns: readonly [];
+        }>[];
+    }>;
+}>;
+declare type AllowedUsedRef = tsql.FromClauseUtil.AllowedUsedRef<typeof query["fromClause"], {
+    isLateral: true;
+}>;
+declare type AllowedExpr = tsql.IExpr<{
+    mapper: () => boolean;
+    usedRef: AllowedUsedRef;
+}>;
+/**
+ * This is allowed because the column will always be `bigint`.
+ * And our `WHERE` clause can handle both `bigint` and `null` cases.
+ */
+export declare const notAllowed: AllowedExpr;
+export {};
